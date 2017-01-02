86ac4cbac1c4ba87549e0181d4a08bec3ed9e970
HaunsTM <hans.baldetorp@gmail.com>
2017-01-02 10:53:12 +0100

    Simple functionality in, proof of concept, in order to turn on/off tellstick devices

---
+++ TestProjectHtml/obj/Release/Package/PackageTmp/lib/TellstickSwitchers.js
@@ -0,0 +1,42 @@
+﻿$('#toggleSwitch').bootstrapToggle({
+    on: 'Tänd',
+    onstyle: 'warning',
+    off: 'Släck'
+});
+
+var hemsamaritenWCFServiceURL = 'http://10.0.0.2:8525/HemsamaritenWCFService/';
+
+var toggleDeviceSwitch = function toggleDeviceSwitch(id) {
+    var ajaxOptions;
+    var turnOnDevice = $('#toggleSwitch').prop('checked');
+    if (turnOnDevice) {
+        ajaxOptions = {
+            async: true,
+            url: hemsamaritenWCFServiceURL + 'TurnOnTellstickDevice',
+            data: 'nativeDeviceId=' + id,
+            dataType: 'jsonp',
+            contentType: "application/json; charset=utf-8",
+            type: 'GET'
+        };
+    } else {
+        ajaxOptions = {
+            async: true,
+            url: hemsamaritenWCFServiceURL + 'TurnOffTellstickDevice',
+            data: 'nativeDeviceId=' + id,
+            dataType: 'jsonp',
+            contentType: "application/json; charset=utf-8",
+            type: 'GET'
+        };
+    }
+    console.debug('Sent: ' + ajaxOptions.url);
+    // Initiate the request!
+    $.ajax(ajaxOptions)
+        .then(
+        function () {
+
+        },
+        function (message) {
+            debugger;
+        }
+    );
+}
