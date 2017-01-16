97134ed584532eeda3286a9be695525b1c580882
HaunsTM <hans.baldetorp@gmail.com>
2017-01-16 18:13:58 +0100

    Added a new .js script file for Surveillance camera Updated build script Added a temporary hard code URL for a surveillance camera for testing purpose

---
+++ TestProjectHtml/lib/SurveillanceCameraModal.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+var surveillanceCamera = [
+    {
+        id: 0,
+        requestUrl: 'http://10.0.0.70:8080/',
+        snapshot: 'shot.jpg',
+        videoStream: 'video',
+        ledFlashlight: 'enabletorch'
+    },
+    {
+        id: 1,
+        requestUrl: 'http://10.0.0.72:8080/',
+        snapshot: 'shot.jpg',
+        videoStream: 'video',
+        ledFlashlight: 'enabletorch'
+    }];
+var showSurveillanceCamera = function() {
+    $("#surveillanceCameraModal").modal({ show: true });
+}
