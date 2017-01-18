40fe6432af3b99a3c68929d2ad91bbf1c3f85c11
HaunsTM <hans.baldetorp@gmail.com>
2017-01-18 10:32:56 +0100

    Can switch on flaslight, can toggle closest Tellstick device

--- TestProjectHtml/lib/SurveillanceCameraModal.js
+++ TestProjectHtml/lib/SurveillanceCameraModal.js
@@ -1,20 +1,170 @@
 ﻿'use strict';
+var hemsamaritenWCFServiceURL = 'http://10.0.0.2:8525/HemsamaritenWCFService/';
 
-var surveillanceCamera = [
+var selectedCamera = null;
+
+var surveillanceCameras = [
     {
-        id: 0,
+        mapCameraCustomId: 0,
+        name: 'Sony Xperia Z3 Compact',
+        location: 'Hans rum mot skrivbordet',
         requestUrl: 'http://10.0.0.70:8080/',
-        snapshot: 'shot.jpg',
-        videoStream: 'video',
-        ledFlashlight: 'enabletorch'
+        APIMethod: {
+            audio: 'audio.opus',
+            snapshot: 'shot.jpg',
+            videoStream: 'video',
+            ledFlashlight: {
+                curState: 'off',
+                possibleStates: [{ name: 'enabletorch', state: 'off' },
+                                 { name: 'disabletorch', state: 'on' }]
+            }
+        },
+        closestTellstickDevice: { nativeDeviceId: '49', state: 'off' }
     },
     {
-        id: 1,
+        mapCameraCustomId: 1,
+        name: 'Samsung Galaxy S4',
+        location: '',
         requestUrl: 'http://10.0.0.72:8080/',
-        snapshot: 'shot.jpg',
-        videoStream: 'video',
-        ledFlashlight: 'enabletorch'
+        APIMethod: {
+            audio: 'audio.opus',
+            snapshot: 'shot.jpg',
+            videoStream: 'video',
+            ledFlashlight: {
+                curState: 'off'
+            }
+        },
+        closestTellstickDevice: { nativeDeviceId: '40', state: 'off' }
     }];
-var showSurveillanceCamera = function() {
-    $("#surveillanceCameraModal").modal({ show: true });
+
+function SurveillanceCameraIndexBy(mapCameraCustomId) {
+    var i;
+    var m = null;
+    for (var i = 0; i < surveillanceCameras.length; ++i) {
+        if (surveillanceCameras[i].mapCameraCustomId !== mapCameraCustomId)
+            continue;
+        m = surveillanceCameras[i];
+        break;
+    }
+    // m is now either null or the one you want
+    return m != null ? i : null;
 }
+
+function CameraBy(mapCameraCustomId) {
+    var m = null;
+    for (var i = 0; i < surveillanceCameras.length; ++i) {
+        if (surveillanceCameras[i].mapCameraCustomId !== mapCameraCustomId)
+            continue;
+        m = surveillanceCameras[i];
+        break;
+    }
+    // m is now either null or the one you want
+    return m;
+}
+
+function SendGetRequest(APIMethod) {
+    $.get(selectedCamera.requestUrl + APIMethod);
+}
+
+function ToggleTellstickDeviceSwitch(selectedCamera) {
+    var ajaxOptions;
+    var turnOnDevice;
+    var cameraIndex = SurveillanceCameraIndexBy(selectedCamera.mapCameraCustomId);
+    var state = selectedCamera.closestTellstickDevice.state;
+    if (state === 'on') {
+        turnOnDevice = false;
+        surveillanceCameras[cameraIndex].closestTellstickDevice.state = 'off';
+    } else {
+        turnOnDevice = true;
+        surveillanceCameras[cameraIndex].closestTellstickDevice.state = 'on';
+    }
+
+    if (turnOnDevice) {
+        ajaxOptions = {
+            async: true,
+            url: hemsamaritenWCFServiceURL + 'TurnOnTellstickDevice',
+            data: 'nativeDeviceId=' + selectedCamera.closestTellstickDevice.nativeDeviceId,
+            dataType: 'jsonp',
+            contentType: "application/json; charset=utf-8",
+            type: 'GET'
+        };
+    } else {
+        ajaxOptions = {
+            async: true,
+            url: hemsamaritenWCFServiceURL + 'TurnOffTellstickDevice',
+            data: 'nativeDeviceId=' + selectedCamera.closestTellstickDevice.nativeDeviceId,
+            dataType: 'jsonp',
+            contentType: "application/json; charset=utf-8",
+            type: 'GET'
+        };
+    }
+    // Initiate the request!
+    $.ajax(ajaxOptions)
+        .then(
+        function () {
+
+        },
+        function (message) {
+            console.log(message);
+        }
+    );
+}
+
+function ToggleLEDFlashlight(selectedCamera) {
+    var APIMethod;
+    var cameraIndex = SurveillanceCameraIndexBy(selectedCamera.mapCameraCustomId);
+    var state = selectedCamera.APIMethod.ledFlashlight.state;
+
+    if (state === 'on') {
+        APIMethod = 'disabletorch';
+        surveillanceCameras[cameraIndex].APIMethod.ledFlashlight.state = 'off';
+    } else {
+        APIMethod = 'enabletorch';
+        surveillanceCameras[cameraIndex].APIMethod.ledFlashlight.state = 'on';
+    }
+    SendGetRequest(APIMethod);
+}
+
+function TakeAndDownloadSnapshot(selectedCamera) {
+    var tempID = 'tempTakeAndDownloadSnapshotAnchorTag';
+    var snapshotMethod = selectedCamera.requestUrl + selectedCamera.APIMethod.snapshot;
+    var nameOfFileWhenDownloaded = '[SNAPSHOT] ' + selectedCamera.name + '      on' + new Date().toISOString();
+    $('#icoSnapshot').append('<a id="' + tempID + '" href="' + snapshotMethod + '" download></a>');
+    $('#' + tempID).trigger('click');
+    $('#' + tempID).remove();
+
+}
+
+function ToggleAudio(selectedCamera) {
+    
+}
+
+
+function showSurveillanceCamera(id) {
+    selectedCamera = CameraBy(id);
+
+    $('#name').text(selectedCamera.name);
+
+    $('#browser_video').attr('src', selectedCamera.requestUrl + selectedCamera.APIMethod.videoStream);
+
+    $("#surveillanceCameraModal").modal({ show: true });
+
+}
+
+
+$('#icoAudio').on('click', function () {
+    console.debug('Clicked on ToggleAudio.');
+    ToggleAudio(selectedCamera);
+});
+$('#icoSnapshot').on('click', function () {
+    console.debug('Clicked on TakeAndDownloadSnapshot.');
+    TakeAndDownloadSnapshot(selectedCamera);
+});
+$('#icoLEDFlashlight').on('click', function () {
+    console.debug('Clicked on ToggleLEDFlashlight.');
+    ToggleLEDFlashlight(selectedCamera);
+});
+$('#icoClosestTellstickDevice').on('click', function () {
+    console.debug('Clicked on ToggleTellstickDeviceSwitch.');
+    ToggleTellstickDeviceSwitch(selectedCamera);
+});
